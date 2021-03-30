a08bb5a9094109d00d3f80dde4211118708d80c9
Aaron Elgrove <elgrove@github.com>
2021-03-30 17:25:43 +0000

    commit

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime as dt
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,5 +8,15 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
+        # repr tells Python how to print objects of this class
         return '<User {}>'.format(self.username)
-        
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=dt.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}'.format(self.body)
+
